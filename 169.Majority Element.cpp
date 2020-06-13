d1220e24e0e0f25e76f4ae3e60f0efc4cd3a86fb
Jay Singh Talreja <jaysinghtalreja@gmail.com>
2020-06-13 19:29:34 +0530

    Majority Element Added Divide and Conquer Solution

--- 169.Majority Element.cpp
+++ 169.Majority Element.cpp
@@ -54,3 +54,24 @@ public:
         return majority;
     }
 };
+
+//Divide and Conquer
+//Recursively find the majority in the two halves of nums and combine the results. The base case is that the majority element of a single-element array is just that element.
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        return majority(nums, 0, nums.size() - 1);
+    }
+private:
+    int majority(vector<int>& nums, int l, int r) {
+        if (l == r) {
+            return nums[l];
+        }
+        int m = l + (r - l) / 2, lm = majority(nums, l, m), rm = majority(nums, m + 1, r);
+        if (lm == rm) {
+            return lm;
+        }
+        return count(nums.begin() + l, nums.begin() + r + 1, lm) > count(nums.begin() + l, nums.begin() + r + 1, rm) ? lm : rm;
+    }
+}; 
