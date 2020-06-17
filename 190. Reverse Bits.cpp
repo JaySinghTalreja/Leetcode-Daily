bd88ad73dd2a29ef61328bb84285155fe44a5354
Jay Singh Talreja <jaysinghtalreja@gmail.com>
2020-06-17 21:52:35 +0530

    190. Reverse Bits

---
+++ 190. Reverse Bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        unsigned int res = 0;
+        for(int i=0;i<32;i++) {
+            res <<= 1;
+            if((n & 1) == 1) {
+                res |= 1;
+            }
+            n >>= 1; 
+        }
+        return res;
+    }
+};
