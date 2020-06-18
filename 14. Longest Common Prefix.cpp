8151a10a4d9426c0b0aca7b77077ff6ad21da14d
Jay Singh Talreja <jaysinghtalreja@gmail.com>
2020-06-18 18:37:04 +0530

    14. Longest Common Prefix

--- 14. Longest Common Prefix.cpp
+++ 14. Longest Common Prefix.cpp
@@ -12,3 +12,32 @@ public:
         return prefix;
     }
 };
+
+//Time O(n*m*(m+m))
+
+
+//Alternative
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==0)
+        return "";
+        string ans=strs[0];
+        int max=INT_MAX;
+        for(auto& s:strs)
+        {
+            if(s.length()==0)
+            return "";
+            int i=0;
+            for(i=0;i<ans.length()&&i<s.length();i++)
+            {
+                if(s[i]!=ans[i])
+                break;
+            }
+            ans=ans.substr(0,i);
+        }
+
+        return ans;
+    }
+};
+//Time O(n*m)
