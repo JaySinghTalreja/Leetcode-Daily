110721a743373378db85607595df51d38391410d
Jay Singh Talreja <jaysinghtalreja@gmail.com>
2020-06-22 15:53:58 +0530

    160. Intersection of Two Linked List

--- 101. Symmetric Tree.cpp
+++ 101. Symmetric Tree.cpp
@@ -27,3 +27,56 @@ public:
         return check(root->left, root->right);
     }
 };
+
+
+
+//Stack -> DFS
+//Stack can hold null! 
+public boolean isSymmetric(TreeNode root) {
+    if (root == null) return true;
+    Stack<TreeNode> stack = new Stack<>();
+    stack.push(root.left);
+    stack.push(root.right);
+    while (!stack.empty()) {
+        TreeNode n1 = stack.pop(), n2 = stack.pop();
+        if (n1 == null && n2 == null) continue;
+        if (n1 == null || n2 == null || n1.val != n2.val) return false;
+        stack.push(n1.left);
+        stack.push(n2.right);
+        stack.push(n1.right);
+        stack.push(n2.left);
+    }
+    return true;
+}
+
+
+//BFS Iterative
+class Solution {
+public:
+    bool isSymmetric(TreeNode *root) {
+        TreeNode *left, *right;
+        if (!root)
+            return true;
+        
+        queue<TreeNode*> q1, q2;
+        q1.push(root->left);
+        q2.push(root->right);
+        while (!q1.empty() && !q2.empty()){
+            left = q1.front();
+            q1.pop();
+            right = q2.front();
+            q2.pop();
+            if (NULL == left && NULL == right)
+                continue;
+            if (NULL == left || NULL == right)
+                return false;
+            if (left->val != right->val)
+                return false;
+            q1.push(left->left);
+            q1.push(left->right);
+            q2.push(right->right);
+            q2.push(right->left);
+        }
+        return true;
+    }
+};
