e2b818bc0699a404e1c4b04aa65d8999abba5ad3
Jay Singh Talreja <jaysinghtalreja@gmail.com>
2020-06-23 12:57:13 +0530

    349. Intersection of Two Arrays.

---
+++ 349. Intersection of Two Arrays.cpp
@@ -0,0 +1,85 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> s;
+        for(int i=0;i<nums1.size();i++) {
+            s.insert(nums1[i]);
+        }
+        vector<int> c;
+        for(int i=0;i<nums2.size();i++) {
+            auto it = s.find(nums2[i]);
+            if(it  != s.end()) {
+                c.push_back(*it);
+                s.erase(it);
+            }
+        }
+        return c;
+    }
+};
+//TIME : O(M+N)
+//SPACE :O(M) or O(N)
+
+//Alternative SORTING & Two Pointer
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] result = new int[set.size()];
+        int k = 0;
+        for (Integer num : set) {
+            result[k++] = num;
+        }
+        return result;
+    }
+}
+
+// Sorting + Binary Search
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums2);
+        for (Integer num : nums1) {
+            if (binarySearch(nums2, num)) {
+                set.add(num);
+            }
+        }
+        int i = 0;
+        int[] result = new int[set.size()];
+        for (Integer num : set) {
+            result[i++] = num;
+        }
+        return result;
+    }
+    
+    public boolean binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+} 
+
