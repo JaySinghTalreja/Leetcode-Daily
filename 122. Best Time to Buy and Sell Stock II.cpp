a481d993611d511fa9c96f5cb9e72b7af59e383c
Jay Singh Talreja <jaysinghtalreja@gmail.com>
2020-06-24 16:08:59 +0530

    122 Mod

--- 122. Best Time to Buy and Sell Stock II.cpp
+++ 122. Best Time to Buy and Sell Stock II.cpp
@@ -10,6 +10,19 @@ public:
 };
 
 
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        const size_t n = prices.size();
+        int ans = 0;
+        for (size_t i=1;i<n;i++)
+            ans += max(0,prices[i]-prices[i-1]);
+        return ans;
+    }
+}
+
+
 //Tutorial - Errichto
 https://medium.com/@punitkmr/best-time-to-buy-and-sell-stock-ii-7826845144eb
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/discuss/208241/Explanation-for-the-dummy-like-me.
