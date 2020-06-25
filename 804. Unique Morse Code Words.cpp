ce40abe1378a25f39b30b402263626e03e180640
Jay Singh Talreja <jaysinghtalreja@gmail.com>
2020-06-25 12:42:21 +0530

    804. Unique Morse Code Words

---
+++ 804. Unique Morse Code Words.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        vector<string> morse{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        unordered_set<string> codes;
+        for(int i=0;i<words.size();i++) {
+            string sol = "";
+            for(auto ch: words[i]) {
+                sol += morse[ch-'a'];
+            }
+            codes.insert(sol);
+        }
+        return codes.size();
+    }
+};
