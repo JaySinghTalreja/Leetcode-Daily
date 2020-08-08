d4daeb662f24cfc5c09752352a029615096abe7e
Jay Singh Talreja <jaysinghtalreja@gmail.com>
2020-08-08 13:31:37 +0530

    Codechef AUG Long Contest

--- TEMPLATECP.cpp
+++ TEMPLATECP.cpp
@@ -31,7 +31,7 @@ using namespace std;
 #define f1(i, n) for (long long i = 1; i <= n; i++)
 #define loop(i,a,b) for (int i = a; i < b; i++)
 #define rloop(i,a,b) for (int i = a ; i>=b;i--)
-#define tc(t) int t; cin >> t; while (t--)
+#define tc(t) unsigned int t; cin >> t; while (t--)
 #define fast ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 #define maxpq priority_queue<int>
 #define minpq priority_queue<int, vector<int>, greater<int> >
@@ -46,53 +46,22 @@ using namespace std;
 #define umll unordered_map<ll, ll>
 #define trav(it, a) for(auto it = a.begin(); it != a.end(); it++) cout<<*it<<" ";
 
-bool checkSize(map<int, int> &a) {
-    int c=0;
-    //cout<<"MAP:";
-    for(auto D: a) {
-        //cout<<D.first<<" "<<D.second<<endl;
-        if(D.second > 0) {
-            c++;
-        }
-    }
-    return (c == 1) ? true : false; 
-}
 void solve() {
     //Change the return type if necessary
-    map<int, int> c;
-    int n;
-    cin>>n;
-    int num=0;
-    for(int i=0;i<n;i++) {
-        cin>>num;
-        c[num]++;
-    }
-    //checkSize(c);
-    //cout<<"SIZE:"<<c.size();
-    for(auto D: c) {
-        if(c.find(D.first+1) != c.end() && c[D.first+1] != 0) {
-            c[D.first]--;
-        }
-        else if(c.find(D.first-1) != c.end() && c[D.first-1] != 0) {
-            //cout<<"D:"<<D.first<<" "<<D.second;
-            c[D.first]--;
-        }
-        else if(checkSize(c)){
-            cout<<"YES";
-            cout<<endl;
+    long int left, right;
+    cin>>left>>right;
+    while(1) {
+        left = left - right;
+        if(left <= 0) {
+            cout<<"1"<<endl;
             return;
         }
-        else{
-            cout<<"NO";
-            cout<<endl;
+        right = (right == 1) ? 0 :(right/2); 
+        if(right <= 0) {
+            cout<<"0"<<endl;
             return;
         }
     }
-    //checkSize(c);
-    cout<<"NO";
-    cout<<endl;
-    return;
-
 }
 
 int main()
