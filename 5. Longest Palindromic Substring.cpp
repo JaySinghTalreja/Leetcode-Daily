5ae609cec6b1bc66b7dab571d3379790f97ba8fb
Jay Singh Talreja <jaysinghtalreja@gmail.com>
2020-10-04 09:52:47 +0530

    Longest Palindromic Substring

--- 5. Longest Palindromic Substring.cpp
+++ 5. Longest Palindromic Substring.cpp
@@ -63,3 +63,41 @@ public:
     }
 };
 
+//O(N^2)
+class Solution {
+public:
+    string solve(string &S,int i, int j) {
+        string temp = "";
+        if(i==j) {
+            temp = S[i];
+            i--;
+            j++;
+        }
+        
+        while(i>=0 && j < S.length() && S[i] == S[j]) {
+            temp = S[i] + temp + S[j];
+            i--;
+            j++;
+        }
+        return temp;
+    }
+    string longestPalindrome(string S) {
+        if(S.size() == 1) {
+            return S;
+        }
+        string Longest = S.substr(0, 1);
+        for(int i=0;i<S.length();i++) {
+            string temp = solve(S, i, i);
+            if(temp.size() > Longest.size()) {
+                Longest = temp;
+            }
+        }
+        for(int i=0;i<S.length()-1;i++) {
+            string temp = solve(S, i, i+1);
+            if(temp.size() > Longest.size()) {
+                Longest = temp;
+            }
+        }
+        return Longest;
+    }
+};
