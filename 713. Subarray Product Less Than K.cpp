332fc87e1a04c2863f7aad6d4b72182f560f07c5
Jay Singh Talreja <jaysinghtalreja@gmail.com>
2020-10-07 14:18:35 +0530

    713.Subarray Product Less than K

--- 713. Subarray Product Less Than K.cpp
+++ 713. Subarray Product Less Than K.cpp
@@ -23,3 +23,22 @@ public:
     }
     
 };
+
+
+//Much Better Sliding Window Solution
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k == 0) return 0;
+        int cnt = 0;
+        int pro = 1;
+        for (int i = 0, j = 0; j < nums.size(); j++) {
+            pro *= nums[j];
+            while (i <= j && pro >= k) {
+                pro /= nums[i++];
+            }
+            cnt += j - i + 1;
+        }
+        return cnt;
+    }
+};
