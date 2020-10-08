82de8635394ad0dca3d27cc35bc27213fe8ca2b5
Jay Singh Talreja <jaysinghtalreja@gmail.com>
2020-10-08 14:00:33 +0530

    791. Custom Sort String

--- 791. Custom Sort String.cpp
+++ 791. Custom Sort String.cpp
@@ -24,3 +24,39 @@ public:
         return ans;
     }
 };
+
+
+/*
+Use bucket sort to achieve linear time.
+
+Put string T into bucket;
+Scan each character of S and construct the result string using bucket;
+Scan bucket again to append the rest of characters which are not existing in string S.
+Actually, this problem has two follow up questions:
+Follow up 1: If the custom order S is too large, how to solve it efficiently?
+Follow up 2: If the string T is too large, how to solve it efficiently?
+*/
+class Solution {
+    public String customSortString(String S, String T) {
+        int[] bucket = new int[26];
+        for (char c : T.toCharArray()) {
+            bucket[c - 'a']++;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (char c : S.toCharArray()) {
+            for (int i = 0; i < bucket[c - 'a']; i++) {
+                sb.append(c);
+            }
+            bucket[c - 'a'] = 0;
+        }
+        
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < bucket[i]; j++) {
+                sb.append((char) (i + 'a'));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
