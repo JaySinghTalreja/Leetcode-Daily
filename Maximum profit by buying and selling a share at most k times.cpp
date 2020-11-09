398a2b9393db6cbed89454bec8970befdbdd6d98
Jay Singh Talreja <jaysinghtalreja@gmail.com>
2020-11-09 19:06:50 +0530

    Memo + Recur

--- Maximum profit by buying and selling a share at most k times.cpp
+++ Maximum profit by buying and selling a share at most k times.cpp
@@ -2,45 +2,44 @@
 using namespace std;
 int maxProfit = INT_MIN;
 unordered_map<string, int> CACHE;
-int solve(vector<int> &A, int K, int index, int minElement = -1, int profit=0) {
-    string key = to_string(index) + to_string(minElement) + '#' +to_string(profit);  
+int solve(vector<int> &A, int K, int index, bool flag = false) {
+    string key = to_string(index) + '$' + to_string(K) + '$' + to_string(flag);
     //CACHE CHECK
 
     if(K==0) {
 
-        return profit;
+        return 0;
 
     }
 
-    if(index == int(A.size())) {
+    if(index == 0) {
 
-        return profit;
+        return flag ? -A[index] : 0;
 
     }
-
+    
     if(CACHE.find(key) != CACHE.end()) {
         cout<<"\nCache Hits";
         return CACHE[key];
     }
     
-    if(minElement == -1) {
+    if(flag == false) {
 
-        return CACHE[key] = max(solve(A, K, index+1, A[index], profit), solve(A, K, index+1, -1, profit));
+        return CACHE[key] = max(A[index] + solve(A, K, index-1, true), solve(A, K, index-1, flag));
 
     }
-
     else{
 
-        return CACHE[key] = max(solve(A, K-1, index+1, -1, profit + A[index] - minElement), solve(A, K, index+1, minElement, profit));
+        return CACHE[key] = max(-A[index] + solve(A, K-1, index-1, false), solve(A, K, index-1, flag));
 
     }
 }
 int main()
  {
-     vector<int> A{100, 30, 15, 10, 8, 25, 80};
+     vector<int> A{12, 14, 17, 10, 14, 13, 12, 15};
      int K;
      cin>>K;
-     cout<<solve(A, K, 0);
+     cout<<solve(A, K, A.size()-1);
      //maxProfit < 0 ? cout<<0 : cout<<maxProfit;
      return 0;
  }
